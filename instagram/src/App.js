cc6697b044d0d2e58218827e5df83b12e33042e8
conconrob <connortprobertson@gmail.com>
2019-02-20 14:18:40 +0000

    Made a basic login form

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import InsideApp from './components/App/InsideApp'
+import Login from './components/Login/Login'
 import authenticate from './components/authentication/authenticate';
+
 import './App.css'
 
 
 class App extends Component {
-  
+  state = {
+    isLoggedIn: true,
+    username: 'Connor',
+  }
   
   render() {
     const AuthenticateInsideApp = authenticate(InsideApp)
-    return (
-      <div className="App">
-        <AuthenticateInsideApp />
-      </div>
-    );
+    if(this.state.isLoggedIn){
+      return <Login />
+    }
+    else {
+      return (
+        <div className="App">
+          <AuthenticateInsideApp />
+        </div>
+      );
+    } 
   }
 }
 
---
+++ instagram/src/components/Login/Login.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Login.css'
+
+export default class Login extends React.Component {
+    state={
+        usernameInput: '',
+        passwordInput: '',
+    }
+
+    setUser = (event) => {
+        event.preventDefault()
+        localStorage.setItem('username', this.state.usernameInput)
+    }
+    onChange = (event, whatToUpdate) => {
+        this.setState({[whatToUpdate]: event.target.value})
+    }
+
+    render(){
+        return (
+            <div className="login-container">
+                <form onSubmit={this.setUser} className="login-form">
+                    <input
+                        onChange={(event)=>this.onChange(event, 'usernameInput')} 
+                        value={this.state.usernameInput} 
+                        type="text" 
+                        placeholder="Username"/>
+                    <input
+                        onChange={(event)=>this.onChange(event, 'passwordInput')}
+                        value={this.state.passwordInput} 
+                        type="password" 
+                        placeholder="Password"/>
+                    <button>LOGIN</button>
+                </form>
+            </div>
+        )
+    }
+    
+}
