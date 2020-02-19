a5ee1e08ea00beb59bc6c7c7213e9698a65a3a20
Connor Robertson <connortprobertson@gmail.com>
2020-02-18 20:45:54 -0800

    changed homepage

---
+++ precache-manifest.d142d7a9e3bb9b769810c2844d18b7cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2c3e32f33d619b034c7",
+    "url": "/React-Insta-Clone/static/js/runtime~main.d2c3e32f.js"
+  },
+  {
+    "revision": "d617e41b4ab2244f3594",
+    "url": "/React-Insta-Clone/static/js/main.d617e41b.chunk.js"
+  },
+  {
+    "revision": "127f0ce7e44d126413ef",
+    "url": "/React-Insta-Clone/static/js/2.127f0ce7.chunk.js"
+  },
+  {
+    "revision": "d617e41b4ab2244f3594",
+    "url": "/React-Insta-Clone/static/css/main.f5e054b1.chunk.css"
+  },
+  {
+    "revision": "90a2e10d8c20f4088fa0fd0956fcd2ae",
+    "url": "/React-Insta-Clone/index.html"
+  }
+];
