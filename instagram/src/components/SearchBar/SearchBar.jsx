6684d135e6757e31f5e1c1f9949c521c60acd0ac
Connor Robertson <connortprobertson@gmail.com>
2020-02-18 20:31:27 -0800

    to pages

--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,63 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Header =styled.header`
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .search-bar-header {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    .search-bar-header{
-        display: flex;
-        align-items: center;
-    }
-    .fa-instagram{
-        font-size: 30px;
-        padding-right: 20px;
-    }
-    h1 {
-        font-family: 'Grand Hotel', cursive;
-        font-size: 25px;
-        padding-left: 20px;
-        border-left: 1px solid gray;
-        line-height: 1;
-    }
-    .search-bar-search {
-        text-align: center;
-        padding: 4px 0;
-        font-family: fontawesome,-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-        "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-        sans-serif;
-    }
-    .search-bar-search:focus,
-    .search-bar-search:active{
-        outline: none;
-    }
-    .search-bar-buttons {
-        font-size: 20px;
-    }
-    .search-bar-buttons .fa-compass,
-    .search-bar-buttons .fa-heart{
-        padding-right: 15px;
-    }
-`
+  }
+  .fa-instagram {
+    font-size: 30px;
+    padding-right: 20px;
+  }
+  h1 {
+    font-family: 'Grand Hotel', cursive;
+    font-size: 25px;
+    padding-left: 20px;
+    border-left: 1px solid gray;
+    line-height: 1;
+  }
+  .search-bar-search {
+    text-align: center;
+    padding: 4px 0;
+    font-family: fontawesome, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+  }
+  .search-bar-search:focus,
+  .search-bar-search:active {
+    outline: none;
+  }
+  .search-bar-buttons {
+    font-size: 20px;
+  }
+  .search-bar-buttons .fa-compass,
+  .search-bar-buttons .fa-heart {
+    padding-right: 15px;
+  }
+`;
 
-export default function SearchBar({filterPostsByUsername}) {
-    function handleSearchChange(event) {
-        filterPostsByUsername(event.target.value)
-    }
+export default function SearchBar({ filterPostsByUsername }) {
+  function handleSearchChange(event) {
+    filterPostsByUsername(event.target.value);
+  }
 
-    return (
-        <Header>
-            <div className="search-bar-header">
-                <i className="fab fa-instagram"></i>
-                <h1>Instagram</h1>
-            </div>
-            <input onChange={handleSearchChange}  type="text" className="search-bar-search" placeholder='&#xf002; Add Comment' />
-            <div className="search-bar-buttons">
-                <i className="far fa-compass"></i>
-                <i className="far fa-heart"></i>
-                <i className="far fa-user"></i>
-            </div>
-        </Header>
-    )
+  return (
+    <Header>
+      <div className="search-bar-header">
+        <i className="fab fa-instagram"></i>
+        <h1>Instagram</h1>
+      </div>
+      <input
+        onChange={handleSearchChange}
+        type="text"
+        className="search-bar-search"
+        placeholder="&#xf002; Search"
+      />
+      <div className="search-bar-buttons">
+        <i className="far fa-compass"></i>
+        <i className="far fa-heart"></i>
+        <i className="far fa-user"></i>
+      </div>
+    </Header>
+  );
 }
-
---
+++ precache-manifest.11d281d45d064474a97cb84343d608b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b4aba450518804f1b831",
+    "url": "/static/js/main.b4aba450.chunk.js"
+  },
+  {
+    "revision": "4057236647a5b9e4f26a",
+    "url": "/static/js/2.40572366.chunk.js"
+  },
+  {
+    "revision": "b4aba450518804f1b831",
+    "url": "/static/css/main.ddfba1b6.chunk.css"
+  },
+  {
+    "revision": "96ad570451b9bd4b5c699af3da3b9b91",
+    "url": "/index.html"
+  }
+];
